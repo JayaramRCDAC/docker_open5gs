a8084afd5f39ee4a90449e27870a404a63b4e25e
Jayaram R <159750032+JayaramRCDAC@users.noreply.github.com>
2025-02-19 11:14:55 +0530

    Updated vonr deploy script

--- pcscf/pcscf_init.sh
+++ pcscf/pcscf_init.sh
@@ -74,6 +74,14 @@ then
 fi
 
 sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
+
+sed -i 's|SDCORE_NRF_IP|'$SDCORE_NRF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
+sed -i 's|SDCORE_NRF_PORT|'$SDCORE_NRF_PORT'|g' /etc/kamailio_pcscf/pcscf.cfg
+sed -i 's|SDCORE_PCF_IP|'$SDCORE_PCF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
+sed -i 's|SDCORE_PCF_PORT|'$SDCORE_PCF_PORT'|g' /etc/kamailio_pcscf/pcscf.cfg
+sed -i 's|SDCORE_PCF_IP|'$SDCORE_PCF_IP'|g' /etc/kamailio_pcscf/route/mo.cfg
+sed -i 's|SDCORE_PCF_PORT|'$SDCORE_PCF_PORT'|g' /etc/kamailio_pcscf/route/mo.cfg
+
 sed -i 's|SCP_IP|'$SCP_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
 sed -i 's|PCSCF_PUB_IP|'$PCSCF_PUB_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
 sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' /etc/kamailio_pcscf/pcscf.cfg
